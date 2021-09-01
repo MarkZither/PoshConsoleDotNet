c61cd90b44a18590d698f0e37dbbc63ca623c540
MarkZither <mark.burton@zither-it.co.uk>
2021-09-01 21:46:15 +0200

    add .net5 module porject.

---
+++ PoshConsole.Demo.ModuleNet50/Diagnostic/TestActiveDirectoryCmdLet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Management.Automation;
+
+namespace PoshConsole.Demo.ModuleNet50
+{
+    [Cmdlet("Test", "ActiveDirectory")]
+    public class TestActiveDirectoryCmdLet : PSCmdlet
+    {
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            TestForest();
+        }
+
+        private static void TestForest()
+        {
+            Log.Verbose("Testing AD Forest...");
+            var forest = Forest.GetCurrentForest();
+        }
+
+        private static void WalkDomain(System.DirectoryServices.ActiveDirectory.Domain domain)
+        {
+            if (Platform.IsWindows)
+            {
+                Log.Info("Crawling {0}...", domain.Name);
+                DetectNetbiosName(domain);
+                try
+                {
+                    var subDomains = domain.Children;
+                }
+                catch (Exception e)
+                {
+                    Log.Error(string.Format("Failed to retrieve children of {0}", domain.Name), e);
+                }
+            }
+        }
+
+
+        private static void DetectNetbiosName(System.DirectoryServices.ActiveDirectory.Domain domain)
+        {
+            Log.Verbose("Looking up NETBIOS name for {0}", domain.Name);
+        }
+    }
+}
---
+++ PoshConsole.Demo.ModuleNet50/Log.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PoshConsole.Demo.ModuleNet50
+{
+    internal class Log
+    {
+        internal static void Verbose(string v)
+        {
+
+        }
+        internal static void Info(string v)
+        {
+
+        }
+        internal static void Info(string v, string name)
+        {
+
+        }
+
+        internal static void Error(string v, Exception e)
+        {
+
+        }
+
+        internal static void Verbose(string v, string name)
+        {
+
+        }
+
+        internal static void Verbose(string v, string name, string netbiosName)
+        {
+
+        }
+    }
+}
